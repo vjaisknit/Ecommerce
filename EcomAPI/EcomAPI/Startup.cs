55430170c53ffa2b1bab0ab6c37953128d116968
vishal <vjaisknit@gmail.com>
2020-04-12 16:15:15 +0530

    Database

--- EcomAPI/EcomAPI/Startup.cs
+++ EcomAPI/EcomAPI/Startup.cs
@@ -19,7 +19,7 @@ namespace EcomAPI
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
         public IConfiguration Configuration { get; }

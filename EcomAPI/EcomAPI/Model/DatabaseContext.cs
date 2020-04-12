08ae48a869866d0d9371c0312caddb21e32fe5c5
vishal <vjaisknit@gmail.com>
2020-04-12 16:11:31 +0530

    DbContext,Create Category

---
+++ EcomAPI/EcomAPI/Model/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcomAPI.Model
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}

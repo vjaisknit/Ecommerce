a50cf81ba7a51314ee077c975058dc2c8e11921f
vishal <vjaisknit@gmail.com>
2020-04-18 12:51:09 +0530

    Revert "Revert "Part -3""
    This reverts commit d40cb6a6fe7bb5f0d2f1205e86c81c0666a9179a.

--- spa/ecom/src/app/product/product.component.ts
+++ spa/ecom/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  categoryList: Category[];
+
   constructor() { }
 
   ngOnInit() {
+    this.categoryList = [{id: 1, categoryName: 'Fruit', isdelete: 0},
+                         {id: 2, categoryName: 'Vegitable', isdelete: 0},
+                         {id: 3, categoryName: 'Bread', isdelete: 0}];
   }
 
 }
